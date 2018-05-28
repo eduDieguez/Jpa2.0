b480bf41af8d0347316d2441913bdbfe7fa881f9
edudieguez <edudieguez@ProbookHP>
2018-05-28 11:31:04 -0300

    Incluindo anotações de validação dos campos da classe modelo.

--- src/dao/AlterarPaisDAO.java
+++ src/dao/AlterarPaisDAO.java
@@ -1,10 +1,15 @@
 package dao;
 
+import java.util.Set;
 import javax.imageio.spi.ServiceRegistry;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.security.auth.login.Configuration;
+import javax.validation.Constraint;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import modelo.Pais;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
@@ -15,18 +20,22 @@ public class AlterarPaisDAO {
         EntityManager em = emf.createEntityManager();
 
         Pais p = em.find(Pais.class, 3);
-
         p.setIso("ARG");
         p.setNome("Argentina");
 
         em.getTransaction().begin();
-        em.merge(p);
+        Validator validador = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<Pais>> erros = validador.validate(p);
+        if (erros.size() > 0) {
+            for (ConstraintViolation<Pais> erro : erros) {
+                System.out.println("Erro: " + erro.getMessage());
+            }
+        } else {
+            em.merge(p);
+        }
         em.getTransaction().commit();
         em.close();
         emf.close();
-
         System.out.println("Pais: " + p.getNome());
-
     }
-
 }
